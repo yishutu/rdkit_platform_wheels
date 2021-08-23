9f7cb4a140a743e37076da07f1c059c0adc0b3d0
Chris <44866753+kuelumbus@users.noreply.github.com>
2021-08-23 12:15:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ class BuildRDKit(build_ext_orig):
                     cmds = [
             f'bootstrap.bat --with-libraries=python,serialization,iostreams,system,regex --with-python={sys.executable} --with-python-root={Path(sys.executable).parent}/..',
             f'./b2 install -a --with-python --with-serialization --with-iostreams --with-system --with-regex --prefix={boost_install_path} -j 20' \
-            f'-s ZLIB_LIBRARY_PATH=C:\vcpkg\packages\zlib_x86-windows\lib -s ZLIB_INCLUDE=C:\vcpkg\packages\zlib_x86-windows\include',                        
+            f'-s ZLIB_LIBRARY_PATH="C:\vcpkg\packages\zlib_x86-windows\lib" -s ZLIB_INCLUDE="C:\vcpkg\packages\zlib_x86-windows\include"',                        
          ]
 
         # What a dirty hack! :/
